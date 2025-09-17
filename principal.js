633f0abe97b576150db508e188d38ea27b98503f
Larissadods <larissa.pereira.araujo@escola.pr.gov.br>
2025-09-17 13:43:32 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -9,5 +9,10 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura")
 var altura = tdAltura.textContent;
 
-var imc = peso / altura * altura; // 100 / 2 * 2 = 25
-console.log(imc)
+var tdImc = document.querySelector(".info-imc")
+if(peso > 1000 && altura > 3.00) {
+  tdImc.textContent = "Erro";
+} else {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
