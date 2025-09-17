bd3e0a2c4aff8a0242dcbe969658c153289bf1c8
Larissadods <larissa.pereira.araujo@escola.pr.gov.br>
2025-09-17 11:52:12 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -1,3 +1,13 @@
-console.log("fui carregado de um arquivo externo");
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista"; // muda o titulo
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura")
+var altura = tdAltura.textContent;
+
+var imc = peso / altura * altura; // 100 / 2 * 2 = 25
+console.log(imc)
