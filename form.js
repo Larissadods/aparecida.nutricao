9e64833bdf57c25836f5e3df007c794a3214054c
Larissadods <larissa.pereira.araujo@escola.pr.gov.br>
2025-10-22 10:09:09 -0300

    Add files via upload

--- form.js
+++ form.js
@@ -19,14 +19,19 @@ botaoAdicionar.addEventListener("click", function(event) {
  tabela.appendChild(pacienteTr);
 
 form.reset();
+var mensagensErro = document.querySelector("#mensagens-erro");
+ul.innerHTML = "";
 
 });
 
 function exibeMensagensDeErros(erros) {
   var ul = document.querySelector("#mensagens-erro");
+  ul.innerHTML = "";
+
   erros.forEach(function(erro) {
     var li = document.createElement("li");
     li.textContent = erro;
+    ul.appendChild(li);
   });
 
 }
@@ -68,6 +73,10 @@ function validaPaciente(paciente) {
 
   var erros = [];
 
+  if(paciente.nome.length == 0) {
+    erros.push("O nome não pode ser em branco");
+  }
+
   if(!validaPeso(paciente.peso)) {
     erros.push("Peso é inválido");
   }
@@ -76,5 +85,16 @@ function validaPaciente(paciente) {
     erros.push("Altura é inválida");
   }
 
+  if(paciente.gordura.length == 0) {
+    erros.push("A gordura não pode ser em branco");
+  }
+
+  if(paciente.peso.length == 0) {
+    erros.push("O peso não pode ser em branco");
+  }
+
+  if(paciente.altura.length == 0) {
+    erros.push("A altura não pode ser em branco");
+  }
   return erros;
 }
